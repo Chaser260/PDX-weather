ae5a71f2038508ff0b47bc1e83a89c8629a6a7e4
Chaser260 <107070355+Chaser260@users.noreply.github.com>
2022-08-13 20:33:26 -0700

    finished cleaning and checked for outliers and bad data

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# PDX-weather
+# PDX Weather Analysis
+
+Prior to moving to Portland in 2011, I lived in Bend, OR. Growing up, I always heard it was constantly dreary and rainy in Portland all year long. However, since moving here, the stories haven't lived up to their hype about the rain. I'm curious to see what the historical precipitation looks like in Portland compared to what it has looked like for the last 10 years, and whether it has been significantly drier than before.
+
+For this analysis, I am going to pull data from the National Centers For Environmental Information for the Portland area and analyze precipitation.
---
+++ pdx_weather.R
@@ -0,0 +1,54 @@
+# load libraries
+library(rvest)
+library(lubridate)
+library(tidyverse)
+library(glue)
+library(janitor)
+library(measurements)
+
+# import data
+pdx_daily <- "https://www.ncei.noaa.gov/pub/data/ghcn/daily/by_station/USW00024229.csv.gz"
+
+pdx_weather <- read_csv(pdx_daily,
+         col_names = c("station", "date", "variable", "value", "a", "b", "c", "d")) %>% # add column headers
+  select(date, variable, value) %>% # remove unwanted variables
+  pivot_wider(names_from = "variable", values_from = "value") %>% 
+  select(date, TMAX, PRCP, SNOW) %>%  # variable as columns
+  mutate(date = ymd(date),
+         TMAX = TMAX/10*(9/5)+32, #convert from tenths of a degree C to degrees F
+         PRCP = PRCP/10/25.4, #convert from tenths of mm to inches
+         SNOW = SNOW/25.4) %>% #convert from mm to inches
+  clean_names() %>% 
+  drop_na(tmax) # drop NA values where temperature data is not recorded
+
+# check for outliers on all variables
+pdx_weather %>% 
+  ggplot(aes(x=date, y=tmax)) +
+  geom_line() 
+
+pdx_weather %>% 
+  ggplot(aes(x=tmax)) +
+  geom_histogram(binwidth = 3) # no noticeable outliers
+
+pdx_weather %>% 
+  ggplot(aes(x=date, y=prcp)) +
+  geom_line()
+
+pdx_weather %>% 
+  ggplot(aes(x=prcp)) +
+  geom_histogram() +
+  scale_y_continuous(limits = c(0, 2500)) # no noticeable outliers
+
+pdx_weather %>% 
+  ggplot(aes(x=date, y=snow)) +
+  geom_line()
+
+pdx_weather %>% 
+  slice_max(n=10, snow) # max snowfall for one day was 14.4 inches on January 21, 1943. 
+
+pdx_weather %>% 
+  ggplot(aes(x=snow)) +
+  geom_histogram() + 
+  scale_y_continuous(limits = c(0, 100)) # no noticeable outliers
+
+
